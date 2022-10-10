05fbd0a6bbbaa06b81eb4bec5a08bea9c904f2a2
radian628 <break.a.radian@gmail.com>
2022-10-10 11:27:11 -0700

    file search highlighting

--- src/panels/FileExplorer.tsx
+++ src/panels/FileExplorer.tsx
@@ -2,24 +2,37 @@ import { useState } from "react";
 import { useAppStore } from "../app-state/StateManager";
 import { StringInput } from "./Common";
 
+function zipper<A,B>(a: A[], b: (i: number) => B) {
+  return a.map((e, i) => (i == a.length - 1) ? [e] : [e, b(i)]).flat(1);
+}
+
 export function FileExplorer() {
   const [fileNameFilter, setFileNameFilter] = useState("");
-  const [tagsFilter, setTagsFilter] = useState([""]);
+  const [tagsFilter, setTagsFilter] = useState("");
   
   const [files] = useAppStore(
     state => [state.state.files]
   );
 
+  const searchTags = tagsFilter.split(",").map(s => s.replace(/^\s|\s$/g, ""));
+
   return <div>
+    <h2>Search</h2>
+
     <label>Name</label>
     <StringInput val={fileNameFilter} setVal={setFileNameFilter}></StringInput>
+    
     <br></br>
-    <label>Tags</label>
-    <StringInput val={fileNameFilter} setVal={setFileNameFilter}></StringInput>
+    
+    <label>Tags (comma-separated)</label>
+    <StringInput val={tagsFilter} setVal={setTagsFilter}></StringInput>
+
+    <h2>Files</h2>
     <table>
       <thead>
         <tr>
           <th>Name</th>
+          <th>Type</th>
           <th>Tags</th>
         </tr>
       </thead>
@@ -29,8 +42,18 @@ export function FileExplorer() {
             .filter(([id, file]) => file.name.match(fileNameFilter))
             .map(([id, file]) => {
               return <tr key={id}>
-                <td>{file.name.split(fileNameFilter)}</td>
+
+                <td>{fileNameFilter
+                  ? zipper(file.name.split(fileNameFilter), i => {
+                    return <span key={i} className="highlighted">{fileNameFilter}</span>
+                  })
+                  : file.name
+                }</td>
+                
+                <td>{file.type}</td>
+
                 <td>{file.tags.map(tag => <span key={tag}>{tag}</span>)}</td>
+
               </tr>
             })
         }
